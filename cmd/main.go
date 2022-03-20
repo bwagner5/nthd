bb55b1a24148886f5b628354e1cdcec17e4c7364
Brandon Wagner <bmwagner10@gmail.com>
2022-03-19 19:44:24 -0500

    releaser stuff

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -15,14 +16,25 @@ import (
 	"github.com/jaypipes/envutil"
 )
 
+var (
+	version string
+	commit  string
+)
+
 type Options struct {
 	DryRun         bool
 	MetadataIP     string
 	MetadataIPMode string
+	Version        bool
 }
 
 func main() {
 	options := MustParseFlags()
+	if options.Version {
+		fmt.Printf("%s\n", version)
+		fmt.Printf("Git Commit: %s\n", commit)
+		os.Exit(0)
+	}
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	ctx := context.Background()
@@ -66,6 +78,7 @@ func MustParseFlags() Options {
 	root.BoolVar(&options.DryRun, "dry-run", envutil.WithDefaultBool("DRY_RUN", false), "Do not take action on events received")
 	root.StringVar(&options.MetadataIP, "metadata-ip", envutil.WithDefault("METADATA_IP", "http://169.254.169.254"), "The IP address of the instance metadata service")
 	root.StringVar(&options.MetadataIPMode, "metadata-ip-mode", envutil.WithDefault("METADATA_IP_MODE", "ipv4"), "IP mode (ipv4 or ipv6) to access the instance metadata service")
+	root.BoolVar(&options.Version, "version", false, "version information")
 	root.Parse(os.Args[1:])
 	return options
 }
---
+++ etc/scripts/preinstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+adduser --system -s /bin/false --user-group nthd
