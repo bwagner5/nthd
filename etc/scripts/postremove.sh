696fec83d169de718581d4b253eff832fc482233
Brandon <wagnerbm@amazon.com>
2022-03-22 12:47:56 -0500

    fix postremove

--- etc/scripts/postremove.sh
+++ etc/scripts/postremove.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+systemctl stop nthd.service || :
+systemctl disable nthd.service || : 
+systemctl daemon-reload || :
+systemctl reset-failed || :
+
 if [[ $(getent passwd nthd) ]]; then
     userdel -r nthd
 fi
